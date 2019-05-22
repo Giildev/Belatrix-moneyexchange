5daffa80da8de0a4a92b13651a5f948818f57e74
Gilberto Marcano <Giildev.exe@gmail.com>
2019-05-22 15:59:49 -0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// Dependencies
+import React from "react";
+
+// Components
+import Home from "./containers/Home";
+
+// Styles
+import "./style.css";
+
+function App() {
+  return <Home />;
+}
+
+export default App;
---
+++ src/containers/Home/index.jsx
@@ -0,0 +1,18 @@
+// Dependencies
+import React, { Component } from "react";
+
+// Components
+
+// Styles
+import "./style.css";
+
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div>Initial Config</div>;
+  }
+}
