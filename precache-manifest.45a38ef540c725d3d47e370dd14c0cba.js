4777b0de274d9301bb72d303cf7f7ace457dd4fb
Gilberto Marcano <Giildev.exe@gmail.com>
2019-05-23 03:42:11 -0400

    Updates

---
+++ precache-manifest.45a38ef540c725d3d47e370dd14c0cba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f8479b5631b1ee2847d520471f79bad",
+    "url": "/Belatrix-moneyexchange/index.html"
+  },
+  {
+    "revision": "6d8acf7943f1167bd921",
+    "url": "/Belatrix-moneyexchange/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "daa62d938d0f87a7d665",
+    "url": "/Belatrix-moneyexchange/static/css/main.6cf50eec.chunk.css"
+  },
+  {
+    "revision": "6d8acf7943f1167bd921",
+    "url": "/Belatrix-moneyexchange/static/js/2.bf2e021d.chunk.js"
+  },
+  {
+    "revision": "daa62d938d0f87a7d665",
+    "url": "/Belatrix-moneyexchange/static/js/main.8ecf9c0c.chunk.js"
+  },
+  {
+    "revision": "301964fc641d5c6615cf",
+    "url": "/Belatrix-moneyexchange/static/js/runtime~main.5cf75c75.js"
+  }
+]);
